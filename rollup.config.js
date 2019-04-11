f7b4361b9ccf3f7a40370a76777ee0cfa4f4e225
Daybrush <daybrush@gmail.com>
2019-04-12 01:43:12 +0900

    chore: first commit

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import builder from "@daybrush/builder";
+
+export default builder([
+  {
+    name: "keycon",
+    input: "src/KeyController.ts",
+    output: "./dist/keycon.js",
+    resolve: true,
+    commonjs: true,
+    external: {
+        "@egjs/component": "eg.Component",
+    },
+  },
+  {
+    name: "keycon",
+    input: "src/KeyController.ts",
+    output: "./dist/keycon.js",
+    resolve: true,
+    commonjs: true,
+    uglify: true,
+    external: {
+        "@egjs/component": "eg.Component",
+    },
+  },
+  {
+    name: "keycon",
+    input: "src/KeyController.ts",
+    output: "./dist/keycon.pkgd.js",
+    resolve: true,
+    commonjs: true,
+  },
+  {
+    name: "keycon",
+    input: "src/KeyController.ts",
+    output: "./dist/keycon.pkgd.min.js",
+    resolve: true,
+    uglify: true,
+    commonjs: true,
+  },
+  {
+    name: "keycon",
+    input: "src/KeyController.ts",
+    output: "./dist/keycon.esm.js",
+    format: "es",
+  },
+]);
---
+++ src/KeyController.ts
@@ -0,0 +1,52 @@
+import Component from "@egjs/component";
+import { names } from "keycode";
+
+const codeData = {
+    "+": "plus",
+    "left command": "meta",
+    "right command": "meta",
+};
+function getKey(keyCode: number) {
+    let key = names[keyCode] || "";
+
+    for (const name in codeData) {
+        key = key.replace(name, codeData[name]);
+    }
+
+    return key.replace(/\s/g, "");
+}
+
+class KeyController extends Component {
+    constructor() {
+        super();
+
+        window.addEventListener("keydown", this.keydownEvent);
+        window.addEventListener("keyup", this.keyupEvent);
+    }
+    public keydown(comb: string, callback: (e: KeyboardEvent) => void) {
+        return this.on(`keydown.${comb}`, callback);
+    }
+    public keyup(comb: string, callback: (e: KeyboardEvent) => void) {
+        return this.on(`keyup.${comb}`, callback);
+    }
+    private triggerEvent(type: "keydown" | "keyup", e: KeyboardEvent) {
+        const key = getKey(e.keyCode);
+
+        this.trigger(`${type}.${key}`, {
+            ctrlKey: e.ctrlKey,
+            altKey: e.altKey,
+            shiftKey: e.shiftKey,
+            metaKey: e.metaKey,
+        });
+    }
+    private keydownEvent = (e: KeyboardEvent) => {
+        this.triggerEvent("keydown", e);
+    }
+    private keyupEvent = (e: KeyboardEvent) => {
+        this.triggerEvent("keyup", e);
+    }
+}
+
+export default function keycon() {
+    return new KeyController();
+}
